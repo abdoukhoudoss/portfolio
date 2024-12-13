c9efdff0f6a50287b90053a6fa43cf9ee2a60745
abdoukhoudoss <cisse.abdoukhoudoss@gmail.com>
2024-12-13 20:51:19 +0100

    Configuration GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: resolve(__dirname, 'src') }
-    ]
-  }
+  base: '/mon-portfolio/', 
+  plugins: [react()]
 })
